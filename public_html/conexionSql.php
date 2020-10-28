ca3f6e03430f02b4cfd9de2d98c3a0acd18406db
Carlos Soberanis <jcsru13@gmail.com>
2020-10-28 03:36:49 -0600

    Update: configuracion sql, a mejorar

--- public_html/conexionSql.php
+++ public_html/conexionSql.php
@@ -1,5 +1,5 @@
 <?php
 
     $conexion = new mysqli("localhost","root","josecarlos","creatica");
-
+    //$conexion = new mysqli("localhost","id14773500_user","MUy)?qet|]988~IR","id14773500_creatica");
 ?>
